84f64778f6d9c90af6b846e260a9bab899ac4ab5
Jhon Arias <jhon.arias@mercadolibre.com.co>
2021-10-08 15:43:37 -0500

    ADD project javascript avanced

---
+++ 0x0C-Javascript_advanced/2-function_me.js
@@ -0,0 +1,10 @@
+function welcomeMessage(fullName) {
+    return function closure(fullname) {
+      alert(`Welcome ${fullName}`);
+    };
+  }
+  
+  const guillaume = welcomeMessage("Guillaume");
+  const alex = welcomeMessage("Alex");
+  const fred = welcomeMessage("Fred");
+  
